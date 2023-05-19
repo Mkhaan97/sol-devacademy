cd8b1476fd6ca03fe69138125f9cb89ad8b21271
MUDASSAR KHAN <mudassar_khan@hotmail.se>
2023-05-19 21:52:38 +0200

    improved-importing-function

--- server/app.js
+++ server/app.js
@@ -9,13 +9,14 @@ const port = 8000;
 app.use('/journeys', journeyRoutes);
 
 // Connect to MongoDB
-const uri = "mongodb+srv://mkhan:9wn7nw8o@cluster0.fjhmv.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://mkhan:9wn7nw8o@cluster0.fjhmv.mongodb.net/journeylogs?retryWrites=true&w=majority";
 mongoose.connect(uri)
   .then(() => {
     console.log('Connected to MongoDB Atlas');
 
 
-    // Uncomment the following section to import data from CSV and save to the database
+    // Uncomment the following section to import data from CSV and save to the database IF database is empty.
+    
     // csvImporter.importDataFromCSV()
     //   .then(() => {
     //     console.log('Data import complete');
--- server/csv/csvImporter.js
+++ server/csv/csvImporter.js
@@ -1,20 +1,20 @@
+
 const fs = require('fs');
 const csv = require('csv-parser');
 const Journey = require('../models/Journey');
 
-// CSV import, reads every line and imports the journey to MongoDB if it's longer than 10 seconds and 10 m.
 async function importDataFromCSV() {
   return new Promise((resolve, reject) => {
     const fileNames = ['2021-05.csv', '2021-06.csv', '2021-07.csv']; // Update with your desired file names
-    const importCountLimit = 1000; // Number of documents to import from each file
+    const importCountLimit = 100000; // Limited to 100 000 journeys per file because of storage issues in MongoDB Atlas 
 
-    var importCount = 0; // Counter variable
+    let journeys = []; // Array to store the journeys
 
     const processFile = (fileName) => {
       return new Promise((resolve, reject) => {
         fs.createReadStream(fileName)
           .pipe(csv())
-          .on('data', async (data) => {
+          .on('data', (data) => {
             const journey = {
               Departure: String(data.Departure),
               Return: String(data.Return),
@@ -29,20 +29,20 @@ async function importDataFromCSV() {
             if (
               journey['Covered distance (m)'] >= 10 &&
               journey['Duration (sec.)'] >= 10 &&
-              importCount < importCountLimit
+              journeys.length < importCountLimit
             ) {
-              try {
-                const newJourney = new Journey(journey);
-                await newJourney.save();
-                importCount++;
-              } catch (error) {
-                console.log('Failed to save error', error);
-              }
+              journeys.push(journey);
             }
           })
-          .on('end', () => {
-            console.log('Import complete for', fileName);
-            resolve();
+          .on('end', async () => {
+            try {
+              await Journey.insertMany(journeys); // Insert all the journeys at once
+              console.log('Import complete for', fileName);
+              resolve();
+            } catch (error) {
+              console.log('Failed to save error', error);
+              reject(error);
+            }
           })
           .on('error', (error) => {
             console.log('Failed to parse document', error);
@@ -51,7 +51,6 @@ async function importDataFromCSV() {
       });
     };
 
-    // Process each file
     const processFiles = async () => {
       for (const fileName of fileNames) {
         await processFile(fileName);
@@ -63,7 +62,4 @@ async function importDataFromCSV() {
   });
 }
 
-
-
-
-module.exports = { importDataFromCSV};
+module.exports = { importDataFromCSV };
