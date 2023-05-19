ec2575c114c6c84e270a0ac3f2b1d5e33f9ae563
MUDASSAR KHAN <mudassar_khan@hotmail.se>
2023-05-19 20:21:18 +0200

    pagination

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,78 +1,3 @@
-// import React from 'react';
-// import '../components-styling/main.css'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faBicycle } from '@fortawesome/free-solid-svg-icons';
-
-
-// function Main() {
-//   return (
-//     <div className='homediv'>
-//       <div className="home-nav">
-//         <ul className="homenav-list">
-//           <FontAwesomeIcon id='homeIcon' icon={faBicycle} size='2x'/>
-//           <li>Stations</li>
-//         </ul>
-//         <input id="home-search" type="text" placeholder="Search for stations and see the rides!"/>
-//       </div>
-//       <div className="welcome-div">
-//         <h1>Welcome to Helsinki <br/>Bike App!</h1>
-//       </div>
-//       <div className="ride-div">
-//         <h2 className='ride-text'>See where people have cycled<br/>around Helsinki!</h2>
-//         <table className='ride-table'>
-//           <tbody>
-//             <tr className='table-headers'>
-//               <th>Departure</th>
-//               <th>Destination</th>
-//               <th>Distance</th>
-//               <th>Duration</th>
-//             </tr>
-//             <tr className='table-row'>
-//               <td>Alfreds Futterkiste</td>
-//               <td>Maria Anders</td>
-//               <td>Germany</td>
-//               <td>500</td>
-//             </tr>
-//             <tr className='table-row'>
-//               <td>Centro comercial Moctezuma</td>
-//               <td>Francisco Chang</td>
-//               <td>Mexico</td>
-//               <td>500</td>
-//             </tr>
-//             <tr className='table-row'>
-//               <td>Ernst Handel</td>
-//               <td>Roland Mendel</td>
-//               <td>Austria</td>
-//               <td>500</td>
-//             </tr>
-//             <tr className='table-row'>
-//               <td>Island Trading</td>
-//               <td>Helen Bennett</td>
-//               <td>UK</td>
-//               <td>500</td>
-//             </tr>
-//             <tr className='table-row'>
-//               <td>Laughing Bacchus Winecellars</td>
-//               <td>Yoshi Tannamuri</td>
-//               <td>Canada</td>
-//               <td>500</td>
-//             </tr>
-//             <tr className='table-row'>
-//               <td>Magazzini Alimentari Riuniti</td>
-//               <td>Giovanni Rovelli</td>
-//               <td>Italy</td>
-//               <td>500</td>
-//             </tr>
-//          </tbody>
-//         </table>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Main
-
-
 import React, { useEffect, useState } from 'react';
 import '../components-styling/main.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -80,38 +5,79 @@ import { faBicycle } from '@fortawesome/free-solid-svg-icons';
 
 function Main() {
   const [journeys, setJourneys] = useState([]);
+  const [totalPages, setTotalPages] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     fetchJourneys();
-  }, []);
+  }, [currentPage]);
 
   const fetchJourneys = async () => {
     try {
-      const response = await fetch('http://localhost:8000/journeys'); // Update the endpoint to match your backend API route
+      const response = await fetch(`http://localhost:8000/journeys?limit=100&page=${currentPage}`);
       const data = await response.json();
-      setJourneys(data);
+      setJourneys(data.journeys);
+      setTotalPages(data.totalPages);
+      console.log(data.journeys);
     } catch (error) {
       console.log('Failed to fetch journeys:', error);
     }
   };
 
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  const renderPagination = () => {
+    const pageRange = 2; // Number of pages to show before and after the current page
+    const pages = [];
+
+    for (let i = Math.max(1, currentPage - pageRange); i <= Math.min(currentPage + pageRange, totalPages); i++) {
+      pages.push(
+        <button
+          key={i}
+          onClick={() => handlePageChange(i)}
+          className={`pagination-button ${i === currentPage ? 'active' : ''}`}
+        >
+          {i}
+        </button>
+      );
+    }
+
+    return (
+      <div className="pagination">
+        {currentPage !== 1 && (
+          <button onClick={() => handlePageChange(currentPage - 1)} className="pagination-button">
+            Previous
+          </button>
+        )}
+        {pages}
+        {currentPage !== totalPages && (
+          <button onClick={() => handlePageChange(currentPage + 1)} className="pagination-button">
+            Next
+          </button>
+        )}
+      </div>
+    );
+  };
+
   return (
-    <div className='homediv'>
+    <div className="homediv">
       <div className="home-nav">
         <ul className="homenav-list">
-          <FontAwesomeIcon id='homeIcon' icon={faBicycle} size='2x'/>
+          <FontAwesomeIcon id="homeIcon" icon={faBicycle} size="2x" />
           <li>Stations</li>
         </ul>
-        <input id="home-search" type="text" placeholder="Search for stations and see the rides!"/>
+        <input id="home-search" type="text" placeholder="Search for stations and see the rides!" />
       </div>
       <div className="welcome-div">
-        <h1>Welcome to Helsinki <br/>Bike App!</h1>
+        <h1>Welcome to Helsinki Bike App!</h1>
       </div>
       <div className="ride-div">
-        <h2 className='ride-text'>See where people have cycled<br/>around Helsinki!</h2>
-        <table className='ride-table'>
+        <h2 className="ride-text">See where people have cycled around Helsinki!</h2>
+        <table className="ride-table">
           <thead>
-            <tr className='table-headers'>
+            <tr className="table-headers">
               <th>Departure Station</th>
               <th>Return Station</th>
               <th>Distance</th>
@@ -120,7 +86,7 @@ function Main() {
           </thead>
           <tbody>
             {journeys.map((journey) => (
-              <tr className='table-row' key={journey._id}>
+              <tr className="table-row" key={journey._id}>
                 <td>{journey['Departure station name']}</td>
                 <td>{journey['Return station name']}</td>
                 <td>{(journey['Covered distance (m)'] / 1000).toFixed(2)} km</td>
@@ -129,6 +95,7 @@ function Main() {
             ))}
           </tbody>
         </table>
+        {renderPagination()}
       </div>
     </div>
   );
